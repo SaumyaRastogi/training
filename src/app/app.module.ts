f2ce5d3d9bbd994dde3c6a5046f04e7a0795dd8e
Rastogi <sarastogi@teksystems.com>
2020-01-28 11:46:00 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,29 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StudentFormComponent } from './student-form/student-form.component';
 import { ReactiveformComponent } from './reactiveform/reactiveform.component';
-
+// import { AppRoutingModule} from './app-routing.module';
+import {RouterModule,Route} from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    
-    
     StudentFormComponent,
     ReactiveformComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'reactiveform',
+        component:ReactiveformComponent
+      },
+      {
+        path:'template',
+        component:StudentFormComponent
+      }
+    ])
    
     
   ],
